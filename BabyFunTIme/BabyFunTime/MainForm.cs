e7d4a99910b2af6f3dd0c9cd52f7b403f544cff9
skonakanchi <srkonakanchi@lsac.org>
2020-07-09 13:21:24 -0400

    Implemented core logic for loading the background music, MainForm_KeyPress event to display used pressed key values. Implemented random funciton.Added media files.

--- BabyFunTIme/BabyFunTime/MainForm.cs
+++ BabyFunTIme/BabyFunTime/MainForm.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Media;
 using System.Text;
 using System.Windows.Forms;
 
@@ -11,6 +13,10 @@ namespace BabyFunTime
 {
     public partial class MainForm : Form
     {
+        Random rand = new Random();
+         
+       
+
         public MainForm()
         {
             InitializeComponent();
@@ -18,6 +24,11 @@ namespace BabyFunTime
 
         private void MainForm_Load(object sender, EventArgs e)
         {
+            string basePath = Environment.CurrentDirectory;
+            string relativePath = @"\media\background.wav";
+            var path = Path.GetFullPath(basePath+relativePath);
+            SoundPlayer simpleSound = new SoundPlayer(path);
+            simpleSound.Play();
 
         }
 
@@ -28,7 +39,19 @@ namespace BabyFunTime
 
         private void MainForm_KeyPress(object sender, KeyPressEventArgs e)
         {
-            MessageBox.Show(e.KeyChar.ToString());
+            //MessageBox.Show(e.KeyChar.ToString());
+
+            System.Drawing.Graphics graphicsObj;
+
+            graphicsObj = this.CreateGraphics();
+            Font myFont = new System.Drawing.Font("Helvetica", 40, FontStyle.Italic);
+            Brush myBrush = new SolidBrush(System.Drawing.Color.Red);
+            graphicsObj.DrawString(e.KeyChar.ToString(), myFont, myBrush, Random(rand,0, 1000), Random(rand,50, 1000));
+        }
+
+        public static int Random(Random rand, int min, int max)
+        {
+            return rand.Next(min, max);
         }
     }
 }
